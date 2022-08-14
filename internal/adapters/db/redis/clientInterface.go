22d401d60c1f7b97867066c35428a6440e95d607
Vyacheslav M <vrmolodyakov@mail.ru>
2022-08-14 14:59:25 +0300

    Add simple Save and Get for Redis

--- internal/adapters/db/redis/clientInterface.go
+++ internal/adapters/db/redis/clientInterface.go
@@ -1,1 +1,12 @@
 package redis
+
+import (
+	"context"
+	"time"
+
+	"github.com/go-redis/redis"
+)
+
+type RedisClient interface {
+	Set(context.Context, string, interface{}, time.Duration) *redis.StatusCmd
+}
